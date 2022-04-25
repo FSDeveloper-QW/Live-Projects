7c946f41738fcb2bc95c2e79e848aa3421ba7dda
Quintus <webdevelopment@shopmax.co.za>
2022-04-25 15:45:39 +0200

    .Update 2 (25 April 2022)

---
+++ training_add_to_cart_animation_01/script.js
@@ -0,0 +1,16 @@
+// -----------------------------------------------------------------------------
+// TODO:  SCRIPT TO ADD COUNTER TO SHOPPING CART WHEN "ADD TO CART" IS CLICKED
+// -----------------------------------------------------------------------------
+
+let count = 0;
+
+const counter = document.getElementById("counter");
+document.getElementById("add-animation").addEventListener("click", (event) => {
+  const cl = counter.classList;
+  const c = "animated-counter";
+  count++;
+
+  counter.innerText = count;
+  cl.remove(c, cl.contains(c));
+  setTimeout(() => counter.classList.add("animated-counter"), 1);
+});
---
+++ training_add_to_wishlist_animation_01/script.js
@@ -0,0 +1,18 @@
+// -----------------------------------------------------------------------------
+// TODO:  SCRIPT TO ADD COUNTER TO SHOPPING CART WHEN "ADD TO CART" IS CLICKED
+// -----------------------------------------------------------------------------
+
+let count = 0;
+
+const counter = document.getElementById("wishlist-counter");
+document
+  .getElementById("wishlist-add-animation")
+  .addEventListener("click", (event) => {
+    const cl = counter.classList;
+    const c = "wishlist-animated-counter";
+    count++;
+
+    counter.innerText = count;
+    cl.remove(c, cl.contains(c));
+    setTimeout(() => counter.classList.add("wishlist-animated-counter"), 1);
+  });
