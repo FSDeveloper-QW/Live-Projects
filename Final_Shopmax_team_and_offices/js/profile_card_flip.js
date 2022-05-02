8952238156cb86c26db12a6fba5c029fae28134d
Quintus <webdevelopment@shopmax.co.za>
2022-05-02 22:56:17 +0200

    .Update 2 (2 May 2022)

--- Final_Shopmax_team_and_offices/js/profile_card_flip.js
+++ Final_Shopmax_team_and_offices/js/profile_card_flip.js
@@ -2,9 +2,74 @@
 // TODO:  SCRIPT TO FLIP A PROFILE OR PRODUCT CARD (AND BACK)
 // ==================================================================================
 
+// ----------------------------------------------------------------------------------
+// TODO: TEAM PROFILE CARDS
+// ----------------------------------------------------------------------------------
+
+// ------------------
+// FIXME:  CARD 001
+// ------------------
+
 $(document).ready(function () {
-  $(".animate").click(function () {
-    $(".box").toggleClass("animated");
+  $(".animate-01").click(function () {
+    $(".box-01").toggleClass("animated");
     return false;
   });
 });
+
+// ------------------
+// FIXME:  CARD 002
+// ------------------
+
+$(document).ready(function () {
+  $(".animate-02").click(function () {
+    $(".box-02").toggleClass("animated");
+    return false;
+  });
+});
+
+// ------------------
+// FIXME:  CARD 003
+// ------------------
+
+$(document).ready(function () {
+  $(".animate-03").click(function () {
+    $(".box-03").toggleClass("animated");
+    return false;
+  });
+});
+
+// ------------------
+// FIXME:  CARD 004
+// ------------------
+
+$(document).ready(function () {
+  $(".animate-04").click(function () {
+    $(".box-04").toggleClass("animated");
+    return false;
+  });
+});
+
+// ------------------
+// FIXME:  CARD 005
+// ------------------
+
+$(document).ready(function () {
+  $(".animate-05").click(function () {
+    $(".box-05").toggleClass("animated");
+    return false;
+  });
+});
+
+// ------------------
+// FIXME:  CARD 006
+// ------------------
+
+$(document).ready(function () {
+  $(".animate-06").click(function () {
+    $(".box-06").toggleClass("animated");
+    return false;
+  });
+});
+
+// ------------------------------------------------------------------------------------
