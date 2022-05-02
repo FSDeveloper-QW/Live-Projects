463702dde33d146e7986e71a12bedbea83448dd7
Quintus <webdevelopment@shopmax.co.za>
2022-05-02 03:07:44 +0200

    .Update 1 (2 May 2022)

---
+++ Final_Shopmax_team_and_offices/js/profile_card_flip.js
@@ -0,0 +1,10 @@
+// ==================================================================================
+// TODO:  SCRIPT TO FLIP A PROFILE OR PRODUCT CARD (AND BACK)
+// ==================================================================================
+
+$(document).ready(function () {
+  $(".animate").click(function () {
+    $(".box").toggleClass("animated");
+    return false;
+  });
+});
