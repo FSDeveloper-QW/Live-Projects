62dc59f79576452af8c5f35cc734e081b85787dc
Quintus <webdevelopment@shopmax.co.za>
2022-05-16 02:05:30 +0200

    .Update 1 (16 May 2022)

--- Shopmax_call_to_action/js/calltoaction_product_cards.js
+++ Shopmax_call_to_action/js/calltoaction_product_cards.js
@@ -6,14 +6,84 @@
 // FIXME:  SET VARIABLE - GET FIRST IMAGE BY ID FROM HTML
 // --------------------------------------------------------------------------------------
 
-var container = document.getElementById("featured__product__image");
+var container001 = document.getElementById("featured__product__image__01");
+var container002 = document.getElementById("featured__product__image__02");
+var container003 = document.getElementById("featured__product__image__03");
+var container004 = document.getElementById("featured__product__image__04");
+var container005 = document.getElementById("featured__product__image__05");
+var container006 = document.getElementById("featured__product__image__06");
+var container007 = document.getElementById("featured__product__image__07");
+var container008 = document.getElementById("featured__product__image__08");
+var container009 = document.getElementById("featured__product__image__09");
+var container010 = document.getElementById("featured__product__image__10");
+var container011 = document.getElementById("featured__product__image__11");
+var container012 = document.getElementById("featured__product__image__12");
+var container013 = document.getElementById("featured__product__image__13");
+var container014 = document.getElementById("featured__product__image__14");
+var container015 = document.getElementById("featured__product__image__15");
 
 // --------------------------------------------------------------------------------------
-// FIXME:  FUNCTION TO CHANGE IMAGE IN THE VIARIABLE "CONTAINER"
+// FIXME:  FUNCTION TO CHANGE IMAGE IN THE VARIABLE "CONTAINER"
 // --------------------------------------------------------------------------------------
 
-function change(image) {
-  container.src = image.src;
+function change001(image) {
+  container001.src = image.src;
+}
+
+function change002(image) {
+  container002.src = image.src;
+}
+
+function change003(image) {
+  container003.src = image.src;
+}
+
+function change004(image) {
+  container004.src = image.src;
+}
+
+function change005(image) {
+  container005.src = image.src;
+}
+
+function change006(image) {
+  container006.src = image.src;
+}
+
+function change007(image) {
+  container007.src = image.src;
+}
+
+function change008(image) {
+  container008.src = image.src;
+}
+
+function change009(image) {
+  container009.src = image.src;
+}
+
+function change010(image) {
+  container010.src = image.src;
+}
+
+function change011(image) {
+  container011.src = image.src;
+}
+
+function change012(image) {
+  container012.src = image.src;
+}
+
+function change013(image) {
+  container013.src = image.src;
+}
+
+function change014(image) {
+  container014.src = image.src;
+}
+
+function change015(image) {
+  container015.src = image.src;
 }
 
 // =======================================================================================
@@ -60,225 +130,170 @@ document
 // =======================================================================================
 
 // ---------------------------------------------------------------------------------------
-// TODO: PRODUCT CARDS (LEGAL SERVICES)
+// TODO: PRODUCT CARDS (15 LATEST PRODUCT RELEASES / PROMOTIONS)
 // ---------------------------------------------------------------------------------------
 
 // ---------------------------------------------------------------------------------------
-// FIXME:  CARD 001 - 701_07_01 - LEGAL BOOKKEEPING SERVICES
+// FIXME:  CARD 001 - main_product_promos_01 - APPLE IPAD MINI (5G)
 // ---------------------------------------------------------------------------------------
 
 $(document).ready(function () {
-  $(".animate-701_07_01").click(function () {
-    $(".product-card-701_07_01").toggleClass("animated");
+  $(".animate-promos-01").click(function () {
+    $(".product-card-promos-01").toggleClass("animated");
     return false;
   });
 });
 
 // ---------------------------------------------------------------------------------------
-// FIXME:  CARD 002 - 701_07_02 - ATTORNEY (CIVIL / LITIGATION)
+// FIXME:  CARD 002 - main_product_promos_02 - APPLE MACBOOK M1 PRO 16” LAPTOP
 // ---------------------------------------------------------------------------------------
 
 $(document).ready(function () {
-  $(".animate-701_07_02").click(function () {
-    $(".product-card-701_07_02").toggleClass("animated");
+  $(".animate-promos-02").click(function () {
+    $(".product-card-promos-02").toggleClass("animated");
     return false;
   });
 });
 
 // ---------------------------------------------------------------------------------------
-// FIXME:  CARD 003 - 701_07_03 - ADVOCATE (CIVIL / LITIGATION)
+// FIXME:  CARD 003 - main_product_promos_03 - DELL 32” 4K MONITOR
 // ---------------------------------------------------------------------------------------
 
 $(document).ready(function () {
-  $(".animate-701_07_03").click(function () {
-    $(".product-card-701_07_03").toggleClass("animated");
+  $(".animate-promos-03").click(function () {
+    $(".product-card-promos-03").toggleClass("animated");
     return false;
   });
 });
 
 // ---------------------------------------------------------------------------------------
-// FIXME:  CARD 004 - 701_07_04 - ATTORNEY (LABOUR)
+// FIXME:  CARD 004 - main_product_promos_04 - DELL XPS 15 9520 LAPTOP
 // ---------------------------------------------------------------------------------------
 
 $(document).ready(function () {
-  $(".animate-701_07_04").click(function () {
-    $(".product-card-701_07_04").toggleClass("animated");
+  $(".animate-promos-04").click(function () {
+    $(".product-card-promos-04").toggleClass("animated");
     return false;
   });
 });
 
 // ---------------------------------------------------------------------------------------
-// FIXME:  CARD 005 - 701_07_05 - ADVOCATE (LABOUR)
+// FIXME:  CARD 005 - main_product_promos_05 - HP 290 G4 PC & MONITOR BUNDLE
 // ---------------------------------------------------------------------------------------
 
 $(document).ready(function () {
-  $(".animate-701_07_05").click(function () {
-    $(".product-card-701_07_05").toggleClass("animated");
+  $(".animate-promos-05").click(function () {
+    $(".product-card-promos-05").toggleClass("animated");
     return false;
   });
 });
 
 // ---------------------------------------------------------------------------------------
-// FIXME:  CARD 006 - 701_07_06 - ATTORNEY (CRIMINAL)
+// FIXME:  CARD 006 - main_product_promos_06 - HP ZBOOK FURY 15 G8 LAPTOP
 // ---------------------------------------------------------------------------------------
 
 $(document).ready(function () {
-  $(".animate-701_07_06").click(function () {
-    $(".product-card-701_07_06").toggleClass("animated");
+  $(".animate-promos-06").click(function () {
+    $(".product-card-promos-06").toggleClass("animated");
     return false;
   });
 });
 
 // ---------------------------------------------------------------------------------------
-// FIXME:  CARD 007 - 701_07_07 - ADVOCATE (CRIMINAL)
+// FIXME:  CARD 007 - main_product_promos_07 - HUAWEI MATEBOOK X PRO
 // ---------------------------------------------------------------------------------------
 
 $(document).ready(function () {
-  $(".animate-701_07_07").click(function () {
-    $(".product-card-701_07_07").toggleClass("animated");
+  $(".animate-promos-07").click(function () {
+    $(".product-card-promos-07").toggleClass("animated");
     return false;
   });
 });
 
 // ---------------------------------------------------------------------------------------
-// FIXME:  CARD 008 - 701_07_08 - ATTORNEY (PROPERTY)
+// FIXME:  CARD 008 - main_product_promos_08 - HUAWEI MATEVIEW GT
 // ---------------------------------------------------------------------------------------
 
 $(document).ready(function () {
-  $(".animate-701_07_08").click(function () {
-    $(".product-card-701_07_08").toggleClass("animated");
+  $(".animate-promos-08").click(function () {
+    $(".product-card-promos-08").toggleClass("animated");
     return false;
   });
 });
 
 // ---------------------------------------------------------------------------------------
-// FIXME:  CARD 009 - 701_07_09 - ADVOCATE (PROPERTY)
+// FIXME:  CARD 009 - main_product_promos_09 - HUAWEI P50 PRO
 // ---------------------------------------------------------------------------------------
 
 $(document).ready(function () {
-  $(".animate-701_07_09").click(function () {
-    $(".product-card-701_07_09").toggleClass("animated");
+  $(".animate-promos-09").click(function () {
+    $(".product-card-promos-09").toggleClass("animated");
     return false;
   });
 });
 
 // ---------------------------------------------------------------------------------------
-// FIXME:  CARD 010 - 701_07_10 - ATTORNEY (COMMERCIAL)
+// FIXME:  CARD 010 - main_product_promos_10 - HUAWEI WATCH 3 GT RUNNER
 // ---------------------------------------------------------------------------------------
 
 $(document).ready(function () {
-  $(".animate-701_07_10").click(function () {
-    $(".product-card-701_07_10").toggleClass("animated");
+  $(".animate-promos-10").click(function () {
+    $(".product-card-promos-10").toggleClass("animated");
     return false;
   });
 });
 
 // ---------------------------------------------------------------------------------------
-// FIXME:  CARD 011 - 701_07_11 - ADVOCATE (COMMERCIAL)
+// FIXME:  CARD 011 - main_product_promos_11 - LENOVO TAB P11
 // ---------------------------------------------------------------------------------------
 
 $(document).ready(function () {
-  $(".animate-701_07_11").click(function () {
-    $(".product-card-701_07_11").toggleClass("animated");
+  $(".animate-promos-11").click(function () {
+    $(".product-card-promos-11").toggleClass("animated");
     return false;
   });
 });
 
 // ---------------------------------------------------------------------------------------
-// FIXME:  CARD 012 - 701_07_12 - ATTORNEY (FAMILY)
+// FIXME:  CARD 012 - main_product_promos_12 - LENOVO THINKPAD P15V (GEN 2)
 // ---------------------------------------------------------------------------------------
 
 $(document).ready(function () {
-  $(".animate-701_07_12").click(function () {
-    $(".product-card-701_07_12").toggleClass("animated");
+  $(".animate-promos-12").click(function () {
+    $(".product-card-promos-12").toggleClass("animated");
     return false;
   });
 });
 
 // ---------------------------------------------------------------------------------------
-// FIXME:  CARD 013 - 701_07_13 - ADVOCATE (FAMILY)
+// FIXME:  CARD 013 - main_product_promos_13 - SAMSUNG GALAXY S22 ULTRA (5G)
 // ---------------------------------------------------------------------------------------
 
 $(document).ready(function () {
-  $(".animate-701_07_13").click(function () {
-    $(".product-card-701_07_13").toggleClass("animated");
+  $(".animate-promos-13").click(function () {
+    $(".product-card-promos-13").toggleClass("animated");
     return false;
   });
 });
 
 // ---------------------------------------------------------------------------------------
-// FIXME:  CARD 014 - 701_07_14 - ATTORNEY (PERSONAL INJURY)
+// FIXME:  CARD 014 - main_product_promos_14 - SAMSUNG GALAXY TAB S8 ULTRA (5G)
 // ---------------------------------------------------------------------------------------
 
 $(document).ready(function () {
-  $(".animate-701_07_14").click(function () {
-    $(".product-card-701_07_14").toggleClass("animated");
+  $(".animate-promos-14").click(function () {
+    $(".product-card-promos-14").toggleClass("animated");
     return false;
   });
 });
 
 // ---------------------------------------------------------------------------------------
-// FIXME:  CARD 015 - 701_07_15 - ADVOCATE (PERSONAL INJURY)
+// FIXME:  CARD 015 - main_product_promos_15 - SAMSUNG GALAXY WATCH 4 CLASSIC (LTE) – 46MM
 // ---------------------------------------------------------------------------------------
 
 $(document).ready(function () {
-  $(".animate-701_07_15").click(function () {
-    $(".product-card-701_07_15").toggleClass("animated");
-    return false;
-  });
-});
-
-// ---------------------------------------------------------------------------------------
-// FIXME:  CARD 016 - 701_07_16 - ATTORNEY (SUCCESSION)
-// ---------------------------------------------------------------------------------------
-
-$(document).ready(function () {
-  $(".animate-701_07_16").click(function () {
-    $(".product-card-701_07_16").toggleClass("animated");
-    return false;
-  });
-});
-
-// ---------------------------------------------------------------------------------------
-// FIXME:  CARD 017 - 701_07_17 - ADVOCATE (SUCCESSION)
-// ---------------------------------------------------------------------------------------
-
-$(document).ready(function () {
-  $(".animate-701_07_17").click(function () {
-    $(".product-card-701_07_17").toggleClass("animated");
-    return false;
-  });
-});
-
-// ---------------------------------------------------------------------------------------
-// FIXME:  CARD 018 - 701_07_18 - ATTORNEY (INTELLECTUAL PROPERTY)
-// ---------------------------------------------------------------------------------------
-
-$(document).ready(function () {
-  $(".animate-701_07_18").click(function () {
-    $(".product-card-701_07_18").toggleClass("animated");
-    return false;
-  });
-});
-
-// ---------------------------------------------------------------------------------------
-// FIXME:  CARD 019 - 701_07_19 - ADVOCATE (INTELLECTUAL PROPERTY)
-// ---------------------------------------------------------------------------------------
-
-$(document).ready(function () {
-  $(".animate-701_07_19").click(function () {
-    $(".product-card-701_07_19").toggleClass("animated");
-    return false;
-  });
-});
-
-// ---------------------------------------------------------------------------------------
-// FIXME:  CARD 020 - 701_07_20 - INDEPENDENT CHAIRMAN
-// ---------------------------------------------------------------------------------------
-
-$(document).ready(function () {
-  $(".animate-701_07_20").click(function () {
-    $(".product-card-701_07_20").toggleClass("animated");
+  $(".animate-promos-15").click(function () {
+    $(".product-card-promos-15").toggleClass("animated");
     return false;
   });
 });
