a64c7ff470ec11f28f4bf765ef0cfd939d4152ac
Quintus <webdevelopment@shopmax.co.za>
2022-06-13 14:15:34 +0200

    .Update 1 (13 Jun 2022)

---
+++ products.js
@@ -0,0 +1,62 @@
+// ================================================================================================================
+// TODO:  "PRODUCTS" IS AN ARRAY OF OBJECTS WITH EACH OBJECT REPRESENTING A PRODUCT
+// ================================================================================================================
+
+const products = [
+  // ==============================================================================================================
+  // FIXME:  LATEST PRODUCT RELEASES
+  // ==============================================================================================================
+  {
+    id: 0,
+    imgSrc1: "./images/pic1.png",
+    imgSrc2: "./images/pic2.png",
+    imgSrc3: "./images/pic3.png",
+    imgSrc4: "./images/pic4.png",
+    title: "Product title",
+    descriptionBrief: "short description of product",
+    currency: "R",
+    price: 29.99,
+    imgSrcBrand: "./images/logo.png",
+    Brand: "Acer",
+    Sku: "Mk2",
+    Model: "MK1",
+    inStock: 1,
+    lengthCm: 1,
+    widthCm: 1,
+    heightCm: 1,
+    weightKg: 1,
+    descriptionFull: "long description of product",
+    showProduct: "NO",
+    showPrice: "NO",
+    itemOnSale: "NO",
+    newArrival: "NO",
+  },
+  {
+    id: 0,
+    imgSrc1: "./images/pic1.png",
+    imgSrc2: "./images/pic2.png",
+    imgSrc3: "./images/pic3.png",
+    imgSrc4: "./images/pic4.png",
+    title: "Product title",
+    descriptionBrief: "short description of product",
+    currency: "R",
+    price: 29.99,
+    imgSrcBrand: "./images/logo.png",
+    Brand: "Acer",
+    Sku: "Mk2",
+    Model: "MK1",
+    inStock: 1,
+    lengthCm: 1,
+    widthCm: 1,
+    heightCm: 1,
+    weightKg: 1,
+    descriptionFull: "long description of product",
+    showProduct: "NO",
+    showPrice: "NO",
+    itemOnSale: "NO",
+    newArrival: "NO",
+  },
+  // ==============================================================================================================
+  // FIXME:  MOBILE PRODUCTS - LAPTOPS - APPLE
+  // ==============================================================================================================
+];
