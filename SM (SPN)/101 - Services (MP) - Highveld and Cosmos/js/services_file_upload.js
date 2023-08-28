4d660aa6621acf89d45cdc2935e87226b558131a
Quintus <webdevelopment@shopmax.co.za>
2023-08-28 14:24:36 +0200

    .Update 7 (28 Aug 2023)

---
+++ SM (SPN)/101 - Services (MP) - Highveld and Cosmos/js/services_file_upload.js
@@ -0,0 +1,22 @@
+// ============================================================================
+// TODO:  SCRIPT FOR DISPLAYING SELECTED FILE NAME TO BE UPLOADED
+// ============================================================================
+
+var inputs = document.querySelectorAll(".order__form__file-input");
+
+for (var i = 0, len = inputs.length; i < len; i++) {
+  customInput(inputs[i]);
+}
+
+function customInput(el) {
+  const fileInput = el.querySelector('[type="file"]');
+  const label = el.querySelector("[data-js-label]");
+
+  fileInput.onchange = fileInput.onmouseout = function () {
+    if (!fileInput.value) return;
+
+    var value = fileInput.value.replace(/^.*[\\\/]/, "");
+    el.className += " -chosen";
+    label.innerText = value;
+  };
+}
